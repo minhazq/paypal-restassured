1d8ff763c85fad734c1973e436172120200a98b8
MINHAJUL QURAISHI <mquraishi@MINHAJULs-MBP.home>
2017-04-04 11:25:13 -0400

    First commit

---
+++ src/main/java/mq/com/restassured02/App.java
@@ -0,0 +1,13 @@
+package mq.com.restassured02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/mq/com/restassured02/Test01.java
@@ -0,0 +1,35 @@
+package mq.com.restassured02;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import static org.hamcrest.Matchers.equalTo;
+
+import static io.restassured.RestAssured.given;
+
+public class Test01 {
+
+	
+	@Test
+	public void getAccessToken(){
+		
+		 Response response = given().
+			accept(ContentType.JSON)
+				.header("Accept-Language", "en_US")
+					.contentType("application/x-www-form-urlencoded")
+						.auth().preemptive().basic("ATyfHxBENx8S400y_mxFfS3LpFEYNjOKR8MsJCFACWYutFjsK5pt0eyb5IxXqGv-0GVu3ZY-0liLe4pv", "EOIpohWlqYQrDX2kOzE256fBN2dNjCCfhid7xRPUg58FVW9Tap2QTmN96KnTjL5BmwgL9BXO-JBeY1yh")
+						.formParam("grant_type", "client_credentials")
+						.post("https://api.sandbox.paypal.com/v1/oauth2/token").then().extract().response();
+		
+		
+		
+		System.out.print(response.jsonPath().prettify());
+		
+		
+		
+	}
+}
